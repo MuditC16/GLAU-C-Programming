3609071d47bef68f92ba9dda51d58fab55e0b056
MuditC16 <92314941+MuditC16@users.noreply.github.com>
2022-06-03 13:11:02 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char s1[100],s2[200],i;
+    printf("Enter the string s1");
+    fgets(s1,sizeof(s1),stdin);
+    for(i=0;s1[i]!='\0';++i){
+        s2[i] = s1[i];
+    }
+    s2[i]='\0';
+    printf("string s2 : %s",s2);return 0;
+}
---
+++ while.c
@@ -0,0 +1,87 @@
+/*
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    while (a>1,b>1,c>1){
+        printf("Hi Steeltroops!\n");  
+    }
+    
+}
+*/
+
+
+
+/*
+#include <stdio.h>
+int main(){
+    int i=5,j=6;
+    while(i<=10,j<=10){
+        i++;
+        j++;
+    }
+    printf("%d%d",i,j);
+}
+*/
+
+
+
+/*
+#include <stdio.h>
+int main(){
+    int i=10;
+    while(i>=0){
+    printf("%d\n",i); 
+    i=i-2;
+    }   
+    return 0;
+}
+*/
+
+
+
+/*
+#include <stdio.h>
+int main(){
+    int i = 1;
+    while(i++<=15);
+    printf("%d\n",i);
+}
+*/
+
+
+
+/*
+#include <stdio.h>
+int main(){
+int i = 0;
+    do{
+        printf("GLA");
+        while (i<3);   
+    } 
+}
+*/
+
+
+
+/*
+#include <stdio.h>
+int main(){
+    int j=1;
+    while(j<=255)
+    printf("%d",j);
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+void main(){
+    int i = 0;
+    do{
+        printf("Anand Gupta\n");
+        i=i++;
+        while(i<5);
+    }
+}
+*/
